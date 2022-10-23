64f9f06a4bc3b0548e9f22ebb4c27564ae635e84
Ricardo Soares Sarto <ricardo.sarto@sinch.com>
2022-10-23 12:31:15 -0300

    Enable Mongo Repositories

--- src/main/kotlin/com/up42/codingchallenge/BackendCodingChallengeApplication.kt
+++ src/main/kotlin/com/up42/codingchallenge/BackendCodingChallengeApplication.kt
@@ -2,7 +2,9 @@ package com.up42.codingchallenge
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
+@EnableMongoRepositories
 @SpringBootApplication
 class BackendCodingChallengeApplication
 
