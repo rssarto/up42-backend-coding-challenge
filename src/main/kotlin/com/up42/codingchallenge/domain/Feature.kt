05adad25eebd3a31922ee3562e8782c2bca7bb76
Ricardo Soares Sarto <ricardo.sarto@sinch.com>
2022-10-23 12:34:58 -0300

    Add Feature document class

---
+++ src/main/kotlin/com/up42/codingchallenge/domain/Feature.kt
@@ -0,0 +1,28 @@
+package com.up42.codingchallenge.domain
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import java.util.*
+import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Document("features")
+data class Feature(val properties: Properties?) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    data class Properties(
+        @Field("id")
+        var id: String?,
+        var timestamp: Long?,
+        var acquisition: Acquisition? = null,
+        var quicklook: String?
+    ) {
+
+        @JsonIgnoreProperties(ignoreUnknown = true)
+        data class Acquisition(
+            var beginViewingDate: Long?,
+            var endViewingDate: Long?,
+            var missionName: String?
+        )
+    }
+}
